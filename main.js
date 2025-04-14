137471b447dac88b02bba850c02106b027d6ae76
tipsmenarikid <kim.project25@gmail.com>
2025-04-15 00:22:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.body.innerHTML = `
 <video controls autoplay width="320" height="240">
-  <source src="https://cdn.gdplayer.site/videos/Bokep_ABG_Memek_Bocil_SD_Ini_Sudah_Masuk_2_Jari_Nih_Kenapa_Yah.mp4" type="video/mp4">
+  <source src="" type="video/mp4">
   Browser kamu tidak mendukung video tag.
 </video>
 `;
