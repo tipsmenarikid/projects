1fb89dabb8d3558b96b19714ba7660ac4251f551
tipsmenarikid <kim.project25@gmail.com>
2025-04-22 22:56:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,418 +1,93 @@
-(function loadJQuery(callback) {
-  if (typeof window.jQuery === "undefined") {
-    var script = document.createElement("script");
-    script.src = "https://code.jquery.com/jquery-3.7.0.js";
-    script.onload = callback;
-    document.head.appendChild(script);
-  } else {
-    callback();
-  }
-})(function () {
-
-var videos = [];
-var id = null;
-var index = localStorage.getItem("index");
-var display = 0;
-
-
-var linkArr = ["https://tipsmemarik.github.io"];
-var checkOpenLink = false;
-
-function openLink(index){
-  window.open(linkArr[index], "_blank");
-}
-
-var app = {
-    "start": () => {
-        const style = document.createElement("style");
-        style.textContent = `
-           body {
-             background: black;
-             color: white;
-             font-family: Tahoma;
-            }
-
-      #header{
-         position: fixed;
-         width: 100%;
-         height: 43px;
-         top: 0;
-         left: 0;
-         background: #000;
-         border-bottom: 1px solid #333;
-         z-index: 9999999999;
-       }
-
-       #title{
-          position: absolute;
-          top: 10px;
-          left: 3%;
-          font-size: 20px;
-          font-weight: bold;
-        }
-
-        #video-container {
-            position: fixed;
-            top: 44px;
-            left: 0;
-            width: 100%;
-            background: #000;
-            border-bottom: 1px solid #222;
-            z-index: 9999999999;
-        }
-
-        video {
-            height: 240px;
-            width: 100%;
-        }
-
-        #controls-overlay {
-            position: absolute;
-            top: 25%;
-            left: 0;
-            width: 100%;
-            display: none;
-        }
-
-        #fullscreenBtn {
-            font-size: 15px;
-            font-weight: bold;
-            background: black;
-            color: white;
-            border: 1px solid #555;
-            border-radius: 50%;
-            padding: 2%;
-            opacity: 0.5;
-        }
-
-        #fullscreenBtn:active {
-            background: #222;
-            opacity: 0.8;
-        }
-
-        .controlBtn {
-            font-size: 20px;
-            background: black;
-            border: 1px solid #555;
-            border-radius: 50%;
-            padding: 5%;
-            margin: 5%;
-            opacity: 0.3;
-        }
-
-        .controlBtn:active {
-            background: #bbb;
-            opacity: 0.5;
-        }
-
-        #slider {
-            padding-bottom: 3%;
-        }
-
-        #videoSlider {
-            width: 70%;
-            height: 5px;
-        }
-
-        .time {
-            font-weight: bold;
-        }
-
-        .video-list {
-            position: absolute;
-            top: 360px;
-            width: 95%;
-            margin-top: 20px;
-            background: black;
-        }
-
-        .video-item {
-            background-color: #222;
-            padding: 10px;
-            margin: 5px 0;
-            cursor: pointer;
-            border-radius: 5px;
-            transition: background-color 0.3s;
-            overflow-x: auto;
-        }
-
-        .video-item:hover {
-            background-color: #005bff;
-        }
-
-        .active {
-            background-color: #007bff;
-            color: white;
-        }
-
-        #joinBtn{
-            font-weight: bold;
-            font-size: 16px;
-            padding: 8px;
-            border: none;
-        }
-        `;
-
-document.head.appendChild(style);
-
-
-      const AdsterraPopupScript = document.createElement('script');
-      AdsterraPopupScript.src = '//pl26384878.profitableratecpm.com/7f/e2/a5/7fe2a58f1faa38417e74396208fc6893.js';
-      document.head.appendChild(AdsterraPopupScript);
-
-      const AdsterraSocialBarScript = document.createElement('script');
-      AdsterraSocialBarScript.src = '//pl26391223.profitableratecpm.com/b9/b3/ff/b9b3ff190c057196a98a7f2a94f4c5a4.js';
-      document.head.appendChild(AdsterraSocialBarScript);
-      
-
-        var body = `
-
-<div id="header" class="front"><span id="title">Asupan Bochiel</span></div>
-
-        <div id="video-container">
-            <center>
-                <video id="video">
-                    <source src="" type="video/mp4">
-                </video>
-
-                <div id="controls-overlay">
-                    <button id="fordwardBtn" class="controlBtn">⏮️</button>
-                    <button id="playBtn" class="controlBtn">⏯️</button>
-                    <button id="nextBtn" class="controlBtn">⏭️</button>
-                </div>
-
-                <div id="slider">
-                    &nbsp;<span id="currTime" class="time">0:00</span>
-                    <input id="videoSlider" type="range" value="0" step="1" min="0" max="100" disabled>
-                    <span id="duration" class="time" hidden>0:00</span> &nbsp;
-                    <button id="fullscreenBtn">[ &nbsp;]</button>
-                </div>
-
-                <!--div id="wait">
-                    <span id="waitText"><button id="joinBtn"> >> JOIN OUR TELEGRAM << </button></span><br><br>
-                </div-->
-            </center>
-        </div>
-
-        <div class="video-list">
-            <div id="video-list"></div>
-
-            <br>
-         <center>
-
-         <!-- BANNER SCRIPTS -->
-         <script async="async" data-cfasync="false" src="//pl26391209.profitableratecpm.com/9ccc0d778207fac12bef75d4fe8a7105/invoke.js"></script>
-<div id="container-9ccc0d778207fac12bef75d4fe8a7105"></div>
-
-         </center>
-
-<div id="footer">
-<center>
-<hr>
-<b>&copy; Asupan Bochiel - ${new Date().getFullYear()}</b>
-</center><br>
-</div>
-
-        </div>
-
-        `;
-
-
-        $("body").html(body);
-
-        const video = document.getElementById("video");
-        const videoSlider = document.getElementById("videoSlider");
-
-        $("#video").on("timeupdate", () => {
-            const duration = video.duration;
-            document.getElementById("duration").textContent = formatTime(duration);
-
-            const currTime = video.currentTime;
-            document.getElementById("currTime").textContent = formatTime(currTime);
-
-            if (currTime === video.duration) {
-                $("#video").click();
-            }
-
-            updateSlider();
-
-        });
-
-        videoSlider.addEventListener('input', seekVideo);
-
-        $("#nextBtn").click(() => {
-            if (index < (videos.length - 1)) {
-                index = index + 1;
-                app.load(index);
-            }
-        });
-
-        $("#fordwardBtn").click(() => {
-            if (index > 0) {
-                index = index - 1;
-                app.load(index);
-            }
-        });
-
-        $("#fullscreenBtn").click(() => {
-
-        });
-    },
-
-    "load": (index) => {
-
-        localStorage.setItem("index", index);
-        video.pause();
-
-        const videoURL = videos[index].title;
-        video.src = "https://cdn.gdplayer.site/videos/" + videoURL;
-        video.load();
-        video.play();
-
-        showControls();
-        updateVideoList();
-
-        $("#playBtn").text("◀️");
-      
-        try {
-        if(checkOpenLink === false){
-          openLink(0);
-          checkOpenLink = true;
-        }
-        } catch (err){
-          alert(err);
-        }
+document.head.innerHTML = `
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Asupan Bochiel</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <style>
+    body {
+      font-family: sans-serif;
+      padding: 10px;
     }
-};
-
-function formatTime(seconds) {
-    if (!seconds || isNaN(seconds)) {
-        return "0:00";
-    } else {
-        const minutes = Math.floor(seconds / 60);
-        const secondsRemaining = Math.floor(seconds % 60);
-        return `${minutes}:${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
+    iframe {
+      width: 100%;
+      max-width: 640px;
+      height: 360px;
+      display: block;
+      margin: 0 auto 20px;
     }
-}
-
-function updateSlider() {
-    if (video.duration) {
-        videoSlider.value = (video.currentTime / video.duration) * 100;
+    ul {
+      list-style: none;
+      padding: 0;
     }
-}
-
-function seekVideo() {
-    if (video.duration) {
-        video.currentTime = (videoSlider.value / 100) * video.duration;
+    li {
+      padding: 10px;
+      margin-bottom: 5px;
+      background: #f0f0f0;
+      cursor: pointer;
+      border-radius: 5px;
     }
-}
-
-function createVideoList(id) {
-    const videoList = document.getElementById(id);
-    videoList.innerHTML = '';
-    videos.forEach((video, i) => {
-        const videoItem = document.createElement('div');
-        videoItem.textContent = "Video "+ (i+1); //video.title;
-        videoItem.classList.add('video-item');
-        videoItem.addEventListener('click', () => {
-
-            index = i;
-            app.load(index);
-
-        });
-        videoList.appendChild(videoItem);
-    });
-}
-
-function updateVideoList() {
-    const videoItems = document.querySelectorAll('.video-item');
-    videoItems.forEach((item, i) => {
-        item.classList.remove('active');
-        if (i === index) {
-            item.classList.add('active');
-        }
-    });
-}
+    li:hover {
+      background: #ddd;
+    }
+  </style>
+`;
 
 
-            function showControls(){
-                if (display == 1) {
-                    $("#controls-overlay").hide();
-                    display = 0;
-                } else {
-                    $("#controls-overlay").show();
-                    display = 1;
+document.body.innerHTML = `
+  <h2>Asupan Bochiel</h2>
+  <iframe id="videoFrame" src="" frameborder="0" allowfullscreen></iframe>
+  <ul id="videoList"></ul>
+`;
 
-                    setTimeout(() => {
-                        $("#controls-overlay").hide();
-                        display = 0;
-                    }, 5000);
-                }
-            }
+    const videos = [
+{"url":"https://fastupload.io/video/embed/0412c1cffcdc7bac/640x360/VID_20240502_124902_982.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/3236c05f261a512e/640x360/VID_20240502_125021_750.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/4580725d706730ec/640x360/VID_20240502_125523_158.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/d93d59a5b72dbe1d/640x360/VID_20240507_142541_454.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/bf03d9b8f7388b6a/640x360/VID_20240510_021424_576.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/31225c13f68f60b9/640x360/VID_20240510_021818_643.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/78112a818d9c8e4a/640x360/VID_20240510_021957_909.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/355cd01805fc6259/640x360/VID_20240511_144951_413.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/c344b889fa842dc0/640x360/VID_20240524_162533_410.mp4", "title":""},
+{"url":"https://fastupload.io/video/embed/7c798fdf6b99622f/640x360/VID_20240526_203249_516.mp4", "title":""},
 
 
 
-var link = `https://raw.githubusercontent.com/tipsmenarikid/projects/refs/heads/main/db.js`;
-
-$(document).ready(() => {
-
-    $.ajax({
-        url: link,
-        dataType: "JSON",
-        success: function (data) {
-
-            videos = data;
-
-            app.start();
-
-            $("#video").click(() => {
-                if (display == 1) {
-                    $("#controls-overlay").hide();
-                    display = 0;
-                } else {
-                    $("#controls-overlay").show();
-                    display = 1;
-
-                    setTimeout(() => {
-                        $("#controls-overlay").hide();
-                        display = 0;
-                    }, 5000);
-                }
-            });
-
-            if(!index){
-                localStorage.setItem("index", 0);
-                index = 0;
-            } else {
-                index = Number(index);
-            }
-
-            app.load(index);
-
-            $("#playBtn").click(() => {
-                if (video.paused) {
-                    video.play();
-                    $("#playBtn").text("⏯️");
-                } else {
-                    video.pause();
-                    $("#playBtn").text("◀️");
-                }
-            });
+{"url":"https://fastupload.io/video/embed/e181b7f51175f3c1/640x360/VID_20240502_125021_750.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/ebfc84931b04631c/640x360/VID_20240502_125443_349.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/2b3c6fab950a78d8/640x360/VID_20240502_125507_976.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/be8a62eaa7ad037b/640x360/VID_20240502_131620_086.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/f40fd21a41f08cc0/640x360/VID_20240502_131828_792.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/3cb0dfb2fb655795/640x360/VID_20240503_142643_414.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/72c0f94703547acb/640x360/VID_20240506_135801_018.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/5f6008e7ef1cb6eb/640x360/VID_20240506_140139_258.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/4fe3fd2faecc6753/640x360/VID_20240506_140329_898.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/9d6398f206455cfd/640x360/VID_20240507_142541_454.mp4","title":""},
 
 
 
-$("#joinBtn").click(function(){
-window.open("https://t.me/bokep_bocil_esde", "_blank");
-});
+{"url":"https://fastupload.io/video/embed/62e55e3e64891a74/640x360/VID_20240510_021151_530.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/304e4276f737d456/640x360/VID_20240524_234035_457.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/2e0ebec7d4c1edeb/640x360/5_6271584848473754312.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/7e7c4c490821dc94/640x360/VID-20230324-WA0091.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/688843084e35f1c9/640x360/45ee24c1-1e95-4246-832d-db451c2bb0a1.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/3ff42e69c3d971c1/640x360/5_6271584848473754232.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/dd14f31ef584de65/640x360/5_6271584848473754286.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/67e697b8de6db560/640x360/d83c396f-c8c1-425e-9e37-f9ef3dee3e04.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/3f8d69b7182c6bcd/640x360/VID_20240528_235441_154.mp4","title":""},
+{"url":"https://fastupload.io/video/embed/f8cb2355dfff2f6d/640x360/VID-20230120-WA0027.mp4","title":""}
+];
 
+    const videoList = document.getElementById("videoList");
+    const iframe = document.getElementById("videoFrame");
 
-            createVideoList("video-list");
-            updateVideoList();
-        },
-        error: function () {
-            alert(`Error!`);
-        }
-        });
+    videos.forEach((video, index) => {
+      const li = document.createElement("li");
+      li.textContent = "Video "+ (index+1);
+      li.addEventListener("click", () => {
+        iframe.src = video.url;
+      });
+      videoList.appendChild(li);
     });
 
-});
